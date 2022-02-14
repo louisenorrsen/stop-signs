680981e1cfd63a399debb13ee01e2cc1dce91c22
louisenorrsen <93458801+louisenorrsen@users.noreply.github.com>
2022-02-14 23:37:28 +0100

    Fixes #6

--- script.js
+++ script.js
@@ -9,45 +9,51 @@ const app = Vue.createApp ({
         }
     },
     methods: {
-        // validateSign: function(firstWord, secondWord) {
-        //     let array = ['äta', 'sova']
-            
-
-
-        // },
         generateSign: function() {
-          this.forbidden = this.input.split(/\s+/)
-          const firstWord = this.forbidden[0]
-          const secondWord = this.forbidden[1]
+            this.forbidden = this.input.split(/\s+/)
+            const firstWord = this.forbidden[0]
+            const secondWord = this.forbidden[1]
 
-        //   validatSign(firstWord, secondWord)
+            if (this.forbidden.length > 1) {
+                this.forbidden = `${firstWord[0].toUpperCase() + firstWord.slice(1)} gärna ${secondWord}`
 
-          if (this.forbidden.length > 1) {
-              this.forbidden = `${firstWord[0].toUpperCase() + firstWord.slice(1)} gärna ${secondWord}`
+            } else {
+                const forbiddenActivity = this.input[0].toUpperCase() + this.input.slice(1)
+                this.forbidden = `${forbiddenActivity} gärna`
+            }
 
-          } else {
-              const forbiddenActivity = this.input[0].toUpperCase() + this.input.slice(1)
-              this.forbidden = `${forbiddenActivity} gärna`
-          }
+            this.location = this.location.toLowerCase()
+            const sign = document.getElementById("sign")
+            const button = document.getElementById("button")
+            const random = document.getElementById('random')
+            const submit = document.getElementById("submit")
+            sign.style.display = 'block'
+            sign.style.marginTop = '1em'
+            button.style.display = 'block'
+            submit.style.display = 'none'
+            random.style.display = 'none'
+        },
+        generateRandomSign: function() {
+            const randomPlaces = ['en hall', 'en förskola', 'en skola', 'ett hus', 'en skog', 'ett bibliotek', 'en lekplats']
+            const randomForbiddenActivity = ['spring', 'skrik', 'ha roligt', 'hoppa', 'skratta', 'sola', 'var tjurig']
+            const randomPlacesNumber = Math.floor(Math.random() * randomPlaces.length)
+            const randomActivityNumber = Math.floor(Math.random() * randomForbiddenActivity.length)
 
-          this.location = this.location.toLowerCase()
-          const sign = document.getElementById("sign")
-          const button = document.getElementById("button")
-          const submit = document.getElementById("submit")
-          sign.style.display = "block"
-          sign.style.marginTop ="1em"
-          button.style.display = "block"
-          submit.style.display = "none"
+            this.location = randomPlaces[randomPlacesNumber]
+            this.input = randomForbiddenActivity[randomActivityNumber]
+            this.generateSign()
         },
         resetSign: function() {
             const sign = document.getElementById('sign')
             const button = document.getElementById('button')
             const submit = document.getElementById("submit")
+            const random = document.getElementById('random')
             this.location = ''
             this.input = ['']
             sign.style.display = 'none'
             button.style.display = 'none'
-            submit.style.display = "block"
+            submit.style.display = 'block'
+            random.style.display = 'block'
         }
     }
 })
